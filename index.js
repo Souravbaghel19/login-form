6b0f047625c9389363d1dd50fe8ec0d27cecec97
Souravbaghel19 <bsonu1412@gmail.com>
2021-12-23 23:30:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+function clickme(){
+         //document.getElementsByClassName('gridchild2')[0].innerText='load the extrnal file';
+         //document.getElementsByClassName('gridchild2')[0].style.backgroundColor="blue";
+        //document.write("my first js program")
+        //window.alert("alert msg")
+        //console.log("click me function called");
+        //window.print(); //to preview the page           // book Name for js =eloquent javascript
+       
+     //  let a=5;                              // literals fixed value ex =10,11.25,12,13,14 so on
+     //  let b=6;                              //variable it is used to store rhe value declare variable 
+     //  let c=a+b;                            // constt is can be declare at fixed 
+     //  console.log(c);
+ }
+      /* function doSomething(){
+        let a;
+        a = 5; 
+        var c;
+        c= 6; 
+         ///Arithemathic operator       
+      let product = c*a;                         
+      let sum = c+a;
+      let sub = c-a;
+      let multi = c/a;
+     console.log(product, sum, sub, multi)
+       let b =1;
+       b =b+5;
+
+   // expressions 
+         let  expressionVal =5*b+sum-product;
+          console.log(expressionVal)
+
+         let firstName ="Sourav ";
+         let secondName =" Baghel";
+
+   //string concatenation
+          let resultString =firstName+ " "+ secondName;
+           console.log(resultString);
+}
+           */
+
+
+function variables(){
+	var name ="Sourav";
+	console.log(name)
+
+	var firstName ="firstName",
+	 secondName="secondName",
+	  _class =10;
+	console.log(firstName , secondName ,_class);
+   var myspecialVar=" special";
+   var myspecialVar;
+   console.log(myspecialVar)
+}
